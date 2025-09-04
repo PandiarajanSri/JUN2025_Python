8561d951d3d1e0ddf357442a538c67649534cc75
PandiarajanSri <pandi.rajan93@gmail.com>
2025-09-04 21:08:01 +0530

    variable file is added

---
+++ Udemy_Variable.py
@@ -0,0 +1,11 @@
+'''Variable has LEGB Rule where
+L : Local - Names assigned in any way within a function (def or lambda)
+E : Enclosing functional locals - Names in the local scope of any and all enclosing  functions (def or lambda)m from inner to outer
+G : Global(module) - Names assigned at the top level of a module file or declared global in a def within the file
+B : Built-in (Python) - Names preassigned in the built-in names module : open,range,syntaxError '''
+
+'''Local Variable'''
+
+lambda num : num **2
+
+print(num(10))
