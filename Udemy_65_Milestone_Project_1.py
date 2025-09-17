a1e54467bc0edf6c31dd86bc045b6bbd587b2463
PandiarajanSri <pandi.rajan93@gmail.com>
2025-09-17 22:08:17 +0530

    17SEP_65 Learned

---
+++ Udemy_65_Milestone_Project_1.py
@@ -0,0 +1,18 @@
+'''Updated visual Representation -> User input -> Function -> Updates --> Updated visual Representation'''
+
+'''Displaying the tic tac toe board with function :'''
+
+
+row1 = [' ',' ',' ']
+row2 = [' ',' ',' ']
+row3 = [' ',' ',' ']
+
+row2[1] = 'X'
+
+def display(row1,row2,row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+display(row1,row2,row3)
+
