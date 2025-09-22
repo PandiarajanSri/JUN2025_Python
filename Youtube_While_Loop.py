f956f579b01c511435cc1cba612bd7692b734ff6
PandiarajanSri <pandi.rajan93@gmail.com>
2025-09-22 21:35:25 +0530

    22SEP25 - Revised While loop

--- Youtube_While_Loop.py
+++ Youtube_While_Loop.py
@@ -4,42 +4,61 @@
 #     print(i)
 #     i = i+1
 
-'''1. Print a number from 1 to 5 using while loop'''
-
-# i = 1
+# '''1. Print a number from 1 to 5 using while loop'''
 #
-# while (i < 6):
-#     print(i)
-#     i = i+1
+# # i = 1
+# #
+# # while (i < 6):
+# #     print(i)
+# #     i = i+1
+#
+# '''2. Write a loop statement to print the following series
+# 10,20,30,40,...,200'''
+#
+# # i = 10
+# #
+# # while (i < 201):
+# #     if i % 10 == 0:
+# #         print(i, end=",")
+# #     i = i+1
+#
+# '''3. Write a program to print first 10 natural numbers in reverse order'''
+#
+# # i = 10
+# #
+# # while (i >= 1):
+# #     print(i, end=",")
+# #     i = i - 1
+#
+#
+# '''4. Write a program to find the factorial of a number'''
+#
+# a = int(input("Enter the number to find the factorial : "))
+#
+# fact = 1
+#
+# while (a > 0):
+#     fact = fact * a
+#     a = a - 1
+# print(fact)
 
-'''2. Write a loop statement to print the following series
-10,20,30,40,...,200'''
 
 # i = 10
-#
-# while (i < 201):
-#     if i % 10 == 0:
-#         print(i, end=",")
-#     i = i+1
-
-'''3. Write a program to print first 10 natural numbers in reverse order'''
+# while(i < 201):
+#     print(i,end=",")
+#     i = i + 10
 
 # i = 10
-#
-# while (i >= 1):
-#     print(i, end=",")
+# while(i > 0):
+#     print(i,end=",")
 #     i = i - 1
 
 
-'''4. Write a program to find the factorial of a number'''
-
-a = int(input("Enter the number to find the factorial : "))
-
-fact = 1
-
-while (a > 0):
-    fact = fact * a
-    a = a - 1
-print(fact)
-
-
+# a = int(input("Enter the number to find factorial : "))
+#
+# fact = 1
+#
+# while (a > 0):
+#     fact = fact * a
+#     a = a - 1
+# print(fact)
