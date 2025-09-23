2100cc3ef15b9e2f356b6198c1ad70d7f37eb9d1
PandiarajanSri <pandi.rajan93@gmail.com>
2025-09-23 21:11:50 +0530

    23SEP 69. Is completed

--- Udemy_65_Milestone_Project_1.py
+++ Udemy_65_Milestone_Project_1.py
@@ -23,37 +23,99 @@
 # print(some_value.is_integer())
 
 
-def user_choice():
-    # Variable input
+# def user_choice():
+#     # Variable input
+#
+#     #Initial value check whether it is int or anything else ?
+#     choice = "wrong"
+#     acceptable_value = range(0,11)
+#     within_range = False
+#
+#
+#      #Two Conditions To check
+#     # Is input is Digit or Within Range == False
+#
+#
+#     while choice.isdigit() == False or within_range == False:
+#         choice = input("Please enter the number which is (0-10) : ")
+#
+#         #Digit Check :
+#         if choice.isdigit() == False:
+#             print("Sorry enter number is not a digit")
+#
+#         #Range Check :
+#         if choice.isdigit() == True:
+#             if int(choice) in acceptable_value:
+#                 within_range = True
+#             else:
+#                 print("Sorry entered number is not in range!")
+#                 within_range = False
+#
+#
+#
+#     return choice
+#
+# print(user_choice())
 
-    #Initial value check whether it is int or anything else ?
-    choice = "wrong"
-    acceptable_value = range(0,11)
-    within_range = False
+game_list = [0,1,2]
+
+def display_game(game_list):
+    print("Here is the current list : ")
+    print(game_list)
+
+# display_game(game_list)
+
+def position_choice():
+    choice = 'wrong'
+
+    while choice not in ['0','1','2']:
+
+        choice = input("Pick a position (0,1,2) : ")
+
+        if choice not in ['0','1','2']:
+            print("It is an invalid choice!")
+
+    return int(choice)
+
+# position_choice()
+
+def replacement_choice(game_list,position):
+    user_replacement = input("Type a string to place at position : ")
+
+    game_list[position] = user_replacement
+
+    return game_list
+
+#replacement_choice(game_list,position_choice())
+
+def gameon_choice():
+    choice = 'wrong'
+    while choice not in ['Y', 'N']:
+        choice = input("Keep playing ? (Y or N) : ")
+
+        if choice not in ['Y', 'N']:
+            print("Sorry, I don't understand, Please choose Y or N")
+
+    if choice == 'Y':
+        return True
+    else:
+        return False
 
 
-     #Two Conditions To check
-    # Is input is Digit or Within Range == False
 
+# print(gameon_choice())
 
-    while choice.isdigit() == False or within_range == False:
-        choice = input("Please enter the number which is (0-10) : ")
+game_on = True
+game_list = [0,1,2]
 
-        #Digit Check :
-        if choice.isdigit() == False:
-            print("Sorry enter number is not a digit")
+while game_on == True:
+    display_game(game_list)
 
-        #Range Check :
-        if choice.isdigit() == True:
-            if int(choice) in acceptable_value:
-                within_range = True
-            else:
-                print("Sorry entered number is not in range!")
-                within_range = False
+    position = position_choice()
 
+    game_list = replacement_choice(game_list,position)
 
+    display_game(game_list)
 
-    return choice
-
-print(user_choice())
+    game_on = gameon_choice()
 
